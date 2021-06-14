883ae34dcf811f4f009c7b42cc609caffdb85b1f
Rafael Choque <rchoque.25.07@gmail.com>
2021-06-14 18:11:00 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',/*le da el nombre al selectro q va a salir en index,html */
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+ 
 }
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['spiderman','Ironman','Hulk','Thor','Capitan America' ];
+
+  heroeBorrado: string = '';
+
+  borrarHeroe(){
+    //console.log('borrando...')
+    this.heroeBorrado = this.heroes.shift() || '';
+    
+
+    
+  }
+
+}
